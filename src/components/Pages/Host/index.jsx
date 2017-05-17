64c0c7bde2c9ff925bf90c916505dc554c2cda90
Jie Meng <jiemeng@thoughtworks.com>
2017-05-17 22:52:30 +0800

    Add Host page; Refactor Router path

---
+++ src/components/Pages/Host/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Host.scss'
+
+class Host extends React.Component {
+    render(){
+      return (
+        <div>
+            <h1>Host</h1>
+        </div>
+      )
+    }
+}
+
+export default Host;
