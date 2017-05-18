32797f9b8dc041f33e37ad143089b7ddbe4fbfe6
Jie Meng <jiemeng@thoughtworks.com>
2017-05-18 16:49:29 +0800

    Modify packages script, image in path are well loaded

--- src/components/Pages/Home/index.jsx
+++ src/components/Pages/Home/index.jsx
@@ -9,9 +9,9 @@ class Home extends React.Component {
             <div>
                 <Card style={{boxShadow: '0 2px 4px rgba(0,0,0,.15)', width: '50%'}}>
                     <CardMedia overlay={<CardTitle title="My Kitchen" style={{padding: '0px',}} titleStyle={{fontSize: '18px',paddingLeft: '10px'}}/>}>
-                        <img src="www/images/kitchen_chef1.jpg" style={{height: '210px',}}/>
+                        <img src="images/kitchen/kitchen0.jpg" style={{height: '210px',}}/>
                     </CardMedia>
-                    <CardHeader avatar="www/images/chef1.png" style={{padding: '0px',float: 'right', width: '40px', margin: '2px 2px 0',}}/>
+                    <CardHeader avatar="images/avatar/cook6.png" style={{padding: '0px',float: 'right', width: '40px', margin: '2px 2px 0',}}/>
                     <CardTitle style={{padding: '4px', borderBottom: 'solid 0.2px #dce0e3',}} title="Dinner"
                                titleStyle={{fontSize: '17px'}}/>
 
