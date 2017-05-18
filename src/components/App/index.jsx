060896ae26536a240d2ed48c2f342b65cdd51397
jiao.cui <jiao.cui@rea-group.com>
2017-05-18 20:21:44 +0800

    add  to add host activity

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -5,6 +5,9 @@ import AppBar from 'material-ui/AppBar';
 import classNames from 'classnames/bind';
 import styles from './App.scss';
 import ModelGenerator from '../../utils/ModelGenerator';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import { hashHistory } from 'react-router'
 
 const cx = classNames.bind(styles);
 
@@ -16,6 +19,9 @@ class App extends React.Component {
                     <AppBar title="Share Dining Table"
                             iconClassNameRight="muidocs-icon-navigation-expand-more" style={{backgroundColor: '#FFA000'}}/>
                     {this.props.children}
+                    <FloatingActionButton style={{marginRight: '20px'}} onClick={() => {hashHistory.push({pathname: '/host'}) }}>
+                        <ContentAdd />
+                    </FloatingActionButton>
                 </div>
             </MuiThemeProvider>
         )
