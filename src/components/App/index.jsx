053f9fd28aa19f3cbbf944be0747c9e9b00446a4
jiao.cui <jiao.cui@rea-group.com>
2017-05-18 10:08:49 +0800

    move AppBar into template

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
 import classNames from 'classnames/bind';
 import styles from './App.scss'
 
 const cx = classNames.bind(styles);
 
 class App extends React.Component {
-    render(){
-      return (
-        <MuiThemeProvider>
-          {this.props.children}
-        </MuiThemeProvider>
-      )
+    render() {
+        return (
+            <MuiThemeProvider>
+                <div>
+                    <AppBar title="Share Dinning Table"
+                            iconClassNameRight="muidocs-icon-navigation-expand-more"/>
+                    {this.props.children}
+                </div>
+            </MuiThemeProvider>
+        )
     }
 }
 
--- src/components/Pages/Home/index.jsx
+++ src/components/Pages/Home/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import AppBar from 'material-ui/AppBar';
 import styles from './Home.scss'
 import Card from './Card'
 
@@ -7,10 +6,6 @@ class Home extends React.Component {
     render(){
       return (
         <div>
-            <AppBar
-                title="Title"
-                iconClassNameRight="muidocs-icon-navigation-expand-more"
-            />
             <Card />
         </div>
       )
